9d8f354b800e8e73814413e160128696e73abed8
uplRajdwip <rajdwip.dey@utplco.com>
2022-08-12 16:40:10 +0530

    update variant

--- src/components/textfieldinput/Index.tsx
+++ src/components/textfieldinput/Index.tsx
@@ -1,20 +1,52 @@
 import React from 'react'
 
 const TextFieldInput = (props: any) => {
+
   return (
     <>
-    <p className={props.labelClass}>{props.label}</p>
-    <input 
-    id={props.id}
-    value={props.value}
-    name={props.name}
-    className={props.rootClass} 
-    type={props.type} 
-    placeholder={props.placeholder} 
-    onChange={props.onChange}
-    disabled={props.disabled}
-    {...props}
-    />
+      {props.default &&
+        <>
+          <p className={props.labelClass}>{props.textLabel}</p>
+          <input
+            id={props.id}
+            value={props.value}
+            name={props.name}
+            className={props.rootClass}
+            type='text'
+            placeholder={props.placeholder}
+            onChange={props.onChange}
+            disabled={props.disabled}
+            {...props}
+          />
+        </>}
+
+      {props.underLine &&
+        <>
+          <p className={props.labelClass}>{props.textLabel}</p>
+          <input
+            style={{
+              padding: ' 8px 0px',
+              boxSizing: 'border-box',
+              borderBottom: '1px solid #ffffff',
+              borderTop: 'none',
+              borderLeft: 'none',
+              borderRight: 'none',
+              outline: 'none',
+              background: 'transparent',
+              WebkitTransition: '0.5s',
+              transition: '0.5s'
+            }}
+            id={props.id}
+            value={props.value}
+            name={props.name}
+            className={props.rootClass}
+            type='text'
+            placeholder={props.placeholder}
+            onChange={props.onChange}
+            disabled={props.disabled}
+            {...props}
+          />
+        </>}
     </>
   )
 }
