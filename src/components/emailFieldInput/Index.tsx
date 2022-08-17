174d9994724bc355ddf7984681f11626bf3d728b
uplShreya <shreya.saha@utplco.com>
2022-08-17 18:26:28 +0530

    style change

--- src/components/emailFieldInput/Index.tsx
+++ src/components/emailFieldInput/Index.tsx
@@ -6,12 +6,49 @@
 import React from "react";
 // import { TextField } from "@mui/material";
 import PropTypes from "prop-types";
-// import { EmailStartIcon } from "../../common/svg_files";
-// import { EmailStartIcon } from "../../common/svg_files";
+// import { EmailStartIcon } from "../emailFieldInput/image/svg_files";
 // import styles from "./Index.module.css";
 
 const EmailField = (props: any) => {
-    //   const emailStartSvg = React.useState(EmailStartIcon);
+    // const emailStartSvg = useState(EmailStartIcon);
+
+    const outlinedTheme = {
+        minHeight: '50px',
+        minWidth: '200px',
+        border: props.error ? '1px solid #ff0505' : '1px solid #000000',
+        outline: 'none'
+    }
+
+    const standardTheme = {
+        minHeight: '50px',
+        minWidth: '200px',
+        boxSizing: 'border-box',
+        borderBottom: props.error ? '1px solid #ff0505' : '1px solid #000000',
+        borderTop: 'none',
+        borderLeft: 'none',
+        borderRight: 'none',
+        outline: 'none',
+        background: 'transparent',
+        WebkitTransition: '0.5s',
+        transition: '0.5s'
+    }
+
+    const filledTheme = {
+        minHeight: '50px',
+        minWidth: '200px',
+        boxSizing: 'border-box',
+        borderBottom: props.error ? '1px solid #ff0505' : '1px solid #000000',
+        borderTop: 'none',
+        borderLeft: 'none',
+        borderRight: 'none',
+        outline: 'none',
+        background: '#d7d7d74f',
+        WebkitTransition: '0.5s',
+        transition: '0.5s',
+        borderTopLeftRadius: '4px',
+        borderTopRightRadius: '4px'
+    }
+
 
     /**
      * clickEnter for enter button
@@ -31,17 +68,24 @@ const EmailField = (props: any) => {
     return (
         <div>
             <label className={`${props.lableCls}`}>{props.label}</label>
+
             <input
+                style={props.variant === 'standard' ? standardTheme
+                    : props.variant === 'filled' ? filledTheme
+                        : outlinedTheme}
                 id={props.id}
                 value={props.value}
                 name={props.name}
                 className={props.rootClass}
-                type={props.type}
+                type={props.type ?? 'email'}
                 placeholder={props.placeholder}
                 onChange={props.onChange}
                 disabled={props.disabled}
                 {...props}
+
             />
+            <p className={props.helperTextClass} style={{ color: 'red' }}>{props.helperText}</p>
+
         </div>
     );
 };
